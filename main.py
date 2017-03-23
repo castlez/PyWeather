7ac323a802b360f672b9598fb7ae9d38837dcc3e
jonny Castle <castlez@live.com>
2017-03-23 12:53:09 -0700

    a few readability adjustments

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+'''
+Five day Forecast
+copyright (c) 2017 by Jonathan Castle
+
+This is a small python app that queries
+weather information from a REST api
+presented by Yahoo.com, and presents it
+to the user.
+
+'''
+
+# imports
 import urllib.request as request
 import urllib.parse as parse
 import json
@@ -5,15 +17,20 @@ import pprint
 from flask import Flask
 import webbrowser
 
+# setup
 app = Flask(__name__)
 pp = pprint.PrettyPrinter(indent=2)
 
+
+# execute query and present results
 @app.route('/')
 def present_forecast():
-    place = "portland, or"
+    place = "portland, or" # this will eventually be automatic (or maybe a selector)
+
     # build the url
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"portland, or\")"
+    yql_query = "select * from weather.forecast \
+                 where woeid in (select woeid from geo.places(1) where text=\"portland, or\")"
     yql_url = baseurl + parse.urlencode({'q': yql_query}) + "&format=json"
 
     # make request, retrieve response
@@ -30,6 +47,7 @@ def present_forecast():
                      "<br>" + str(day['text'])
     return outstring + '</body></html>'
 
+# run
 if __name__ == '__main__':
     webbrowser.open("http://127.0.0.1:5000/")
     app.run()
